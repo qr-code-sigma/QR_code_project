8b575ab0b04ac3565ee4eac426d2c5386b40854d
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2025-02-23 21:25:48 +0200

    set up database

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    places = models.IntegerField()
+    location = models.CharField(max_length=100)
+    date = models.DateField()
+
+    class Meta:
+        db_table = "events"
+        managed = False
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=90, unique=True)
+    password = models.CharField(max_length=100)
+    status = models.CharField(
+        max_length=10,
+        choices=[('admin', 'Admin'), ('guest', 'Guest'), ('employee', 'Employee')]
+    )
+
+    class Meta:
+        db_table = "users"
+        unique_together = ('first_name', 'last_name')
+        managed = False
+
+
+class UserEvent(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "user_event"
+        unique_together = ('user', 'event')
+        managed = False
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotend
+from dotenv import load_dotenv
 import os
 
-load_dotend()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,11 +97,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'user-qr',
+        'USER': 'postgres',
+        'PASSWORD': 'last29updated',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

dd48ebbe8d003a3b53a1c7ce3dfb13143d17da36
Vitalii <158774939+s0wime@users.noreply.github.com>
2025-03-02 17:00:35 +0200

    feat(backend): add user editing path

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.get_users),
-    path("<int:id>/", views.get_user)
+    path("<int:id>/", views.get_user),
+    path("<int:id>/edit", views.edit_user_view),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,12 +1,13 @@
+import json
+
 from api.models import User
 from django.http import JsonResponse
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_POST
 from django.core.exceptions import ObjectDoesNotExist
 
 
 @require_GET
 def get_user(request, id):
-    print("Hello")
     try:
         user = User.objects.get(id=id)
     except ObjectDoesNotExist:
@@ -24,3 +25,49 @@ def get_users(request):
     users = User.objects.all()
     user_list = [{"username":user.username, "first_name":user.first_name, "last_name":user.last_name} for user in users]
     return JsonResponse({"users":user_list}, status = 200)
+
+@require_POST
+def edit_user_view(request, id):
+    try:
+        user = User.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return JsonResponse({ "error": "User does not exist" }, status = 404)
+
+    data = json.loads(request.body)
+    new_first_name = data.get("first_name")
+    new_last_name = data.get("last_name")
+    new_username = data.get("username")
+    new_password = data.get("password")
+
+    if new_first_name:
+        if len(new_first_name) > 50:
+            return JsonResponse({ "error": "First name is too long" }, status = 400)
+
+    if new_last_name:
+        if len(new_last_name) > 50:
+            return JsonResponse({ "error": "Last name is too long" }, status = 400)
+
+    if new_username:
+        if len(new_username) > 50:
+            return JsonResponse({ "error": "Username is too long" }, status = 400)
+
+        existing_user = User.objects.exclude(pk=id).filter(username=new_username)
+        if existing_user:
+            return JsonResponse({ "error": "User already exists" }, status = 404)
+
+    if new_password:
+        if len(new_password) > 100:
+            return JsonResponse({ "error": "Password is too long" }, status = 400)
+
+
+    if new_first_name is not None:
+        user.first_name = new_first_name
+    if new_last_name is not None:
+        user.last_name = new_last_name
+    if new_username is not None:
+        user.username = new_username
+    if new_password is not None:
+        user.password = new_password
+    user.save()
+
+    return JsonResponse({"details": "User was successfully updated"}, status = 200)

b877c578a69fb643eb426a2c92fbf649988b2c1b
SofiiaChurikova <sonyachurikova@gmail.com>
2025-03-04 00:06:35 +0200

    changed url in front

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/v1',
+    baseURL: 'https://qr-code-project.up.railway.app/api/v1',
     timeout: 1000,
     headers: {'Content-Type': 'application/json'}
 });

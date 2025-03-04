a234fab0fd76048db4d590ff7f9789bf46657605
DAVAdjjjan <159725111+DAVAdjjjan@users.noreply.github.com>
2025-03-04 23:50:20 +0200

    fix protected routes useSelector

--- frontend/src/utils/ProtectedRoutes.jsx
+++ frontend/src/utils/ProtectedRoutes.jsx
@@ -1,14 +1,14 @@
-import { Outlet, Navigate } from 'react-router-dom';
-import {useSelector} from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const ProtectedRoutes = () => {
-    const { isAuthenticated } = useSelector(state => state.user);
+  const { isAuthenticated } = useSelector((state) => state.auth);
 
-    if(isAuthenticated) {
-        return <Outlet />
-    }
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
 
-    return <Navigate to="/login" />
-}
+  return <Navigate to="/login" />;
+};
 
 export default ProtectedRoutes;

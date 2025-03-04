3c4cf9f7a98c88066018182e77b7ae2e42cc7abc
SofiiaChurikova <sonyachurikova@gmail.com>
2025-03-05 00:04:39 +0200

    add info for redis deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,7 +169,7 @@ PASSWORD_RESET_TIMEOUT = 14400
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1", 
+        "LOCATION": os.getenv("REDIS_URL"), 
     }
 }
 

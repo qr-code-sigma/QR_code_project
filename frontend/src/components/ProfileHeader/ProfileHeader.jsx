a9a38a8aa2310a8e9536036add09dd1dee74bf0e
DAVAdjjjan <159725111+DAVAdjjjan@users.noreply.github.com>
2025-03-04 23:57:44 +0200

    add back btn functionality from profile

--- frontend/src/components/ProfileHeader/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./profileHeader.css";
+import { useNavigate } from "react-router-dom";
 
 function ProfileHeader() {
+  const navigate = useNavigate();
   return (
     <header className="profile-header">
       <div className="profile-header-container">
-        <button className="btn-default">
+        <button onClick={() => navigate("/")} className="btn-default">
           <span>Back</span>
         </button>
         <p className="profile-header-text">Name Surname</p>

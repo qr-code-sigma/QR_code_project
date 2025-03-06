0f71dd1e894aadd731650c6b257f4f52f932898d
DAVAdjjjan <159725111+DAVAdjjjan@users.noreply.github.com>
2025-03-06 10:07:10 +0200

    fix truncate logic

--- frontend/src/utils/truncateText.js
+++ frontend/src/utils/truncateText.js
@@ -1,10 +1,8 @@
-const truncate = (str, maxLen) => {
-  if (str) {
-    const spaceDetection = str[maxLen - 1] === " " ? -2 : -1;
-    return str.length > maxLen
-      ? str.slice(0, maxLen - spaceDetection) + "..."
-      : str;
-  }
+const truncate = (str = "", maxLen) => {
+  if (!str || str.length <= maxLen) return str;
+
+  const spaceDetection = str[maxLen - 1] === " " ? 1 : 0;
+  return str.slice(0, maxLen - spaceDetection) + "...";
 };
 
 export default truncate;

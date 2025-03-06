2e7a61925c7aa6caaf612105eecf0d5d5b65d94e
budulka <149436028+budulka@users.noreply.github.com>
2025-03-06 12:38:22 +0200

    remove getcsrf

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -9,32 +9,5 @@ const instance = axios.create({
     withCredentials: true
 });
 
-instance.interceptors.request.use(
-    (config) => {
-      const csrfToken = getCsrfToken();
-      if (csrfToken) {
-        config.headers['X-CSRFToken'] = csrfToken;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-);
-
-
-function getCsrfToken() {
-    if (!document.querySelector('meta[name="csrf-token"]')) {
-      axios.get('https://qr-code-project.up.railway.app/api/v1/auth/csrf-token', { withCredentials: true })
-        .then(response => {
-          const csrfToken = response.data.csrfToken;
-          const metaTag = document.createElement('meta');
-          metaTag.name = 'csrf-token';
-          metaTag.content = csrfToken;
-          document.head.appendChild(metaTag);
-        });
-    }
-    return document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
-  }
 
 export default instance;

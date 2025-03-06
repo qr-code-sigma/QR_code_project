0f305fe7760a49127d7af81cfa1a4564d8a9dfef
Nikitos4443 <Nikitos4443@gmail.com>
2025-03-06 17:48:34 +0200

    change status max_length

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,7 +8,7 @@ class Event(models.Model):
     places = models.IntegerField()
     location = models.CharField(max_length=100)
     date = models.DateField()
-    status = models.CharField(max_length=100)
+    status = models.CharField(max_length=20)
 
     class Meta:
         db_table = "events"

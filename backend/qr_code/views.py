494d391caac18e97d93cda07d853a5e12e262e3b
budulka <azazasapli@gmail.com>
2025-03-06 14:38:06 +0200

    fix qr code url

--- backend/qr_code/views.py
+++ backend/qr_code/views.py
@@ -6,21 +6,22 @@ import qrcode.constants
 from api.models import UserEvent 
 
 
-#i did not use any function pointers in this function because i found it too coomplicated and unneccessary. Also python does not have them but this is secondary reason.
-# 
-@require_POST #requires POST
-def get_qr(request, event_id): #declare function
-    try: #try excpet clause
-        event_user = UserEvent.objects.get(event_id = event_id, user_id = request.user.id) #get user_event
-        qr = qrcode.QRCode( #qr declaration
-            version=1,  #version 1
-            error_correction=qrcode.constants.ERROR_CORRECT_L, #L error correction
-            box_size=10, #box_size = 10
-            border = 4) #border = 4
-        qr.add_data(f'https://qr-code-project.up.railway.app/QR_code_page.html/{event_user.id}') #add data to qr
-        code = qr.get_matrix() #declare code as qr code matrix
-        return JsonResponse({"code":code}, status = 200) #return code and status 200
-    except Exception: #except exception
-        return JsonResponse({"details"}, status = 404) #return 404 if fail
-    #
-# Create your views here.
+
+@require_POST 
+def get_qr(request, event_id): 
+    try: 
+        event_user = UserEvent.objects.get(event_id = event_id, user_id = request.user.id) 
+        print(f"Creating a QR code for invitaion {event_user} for user {request.user.id} on event {event_id}")
+        qr = qrcode.QRCode( 
+            version=1,  
+            error_correction=qrcode.constants.ERROR_CORRECT_L, 
+            box_size=10, 
+            border = 4) 
+        qr.add_data(f'https://qr-code-project.up.railway.app/QR_code_page.html/{event_user.id}') 
+        code = qr.get_matrix() 
+        print(f"Code: {code}") #temporary
+        return JsonResponse({"code":code}, status = 200) 
+    except Exception: 
+        return JsonResponse({"details":"Cound not create QR code"}, status = 404) 
+    
+    

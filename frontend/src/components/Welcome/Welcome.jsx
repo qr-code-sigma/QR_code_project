70e8a6187bdc562c580249c68b750b1e0cb3e15a
DAVAdjjjan <159725111+DAVAdjjjan@users.noreply.github.com>
2025-03-06 12:32:11 +0200

    delete duplicated navigate

--- frontend/src/components/Welcome/Welcome.jsx
+++ frontend/src/components/Welcome/Welcome.jsx
@@ -1,43 +1,40 @@
 import React from "react";
 import "./welcome.css";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Welcome() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const navigateToRegistration = () => {
-        navigate("/registration");
-    }
-    const navigateToAuthorization = () => {
-        navigate("/authorization");
-    }
+  const navigateToRegistration = () => {
+    navigate("/registration");
+  };
 
-    const navigateToAuthorization = () => {
-        navigate("/authorization");
-    }
+  const navigateToAuthorization = () => {
+    navigate("/authorization");
+  };
 
-    return (
-        <main className="main-welcome-page">
-            <h1>Welcome</h1>
-            <div>
-                <p>
-                    Create an account to register for events and generate your
-                    personalized QR code. Already have an account?
-                </p>
-                <p>
-                    <b> Sign in</b> to access your bookings.
-                </p>
-            </div>
-            <div className="button-container">
-                <button onClick={navigateToRegistration} className="btn-default">
-                    <span>Sign Up</span>
-                </button>
-                <button onClick={navigateToAuthorization} className="btn-primary">
-                    <span>Sign In</span>
-                </button>
-            </div>
-        </main>
-    );
+  return (
+    <main className="main-welcome-page">
+      <h1>Welcome</h1>
+      <div>
+        <p>
+          Create an account to register for events and generate your
+          personalized QR code. Already have an account?
+        </p>
+        <p>
+          <b> Sign in</b> to access your bookings.
+        </p>
+      </div>
+      <div className="button-container">
+        <button onClick={navigateToRegistration} className="btn-default">
+          <span>Sign Up</span>
+        </button>
+        <button onClick={navigateToAuthorization} className="btn-primary">
+          <span>Sign In</span>
+        </button>
+      </div>
+    </main>
+  );
 }
 
 export default Welcome;

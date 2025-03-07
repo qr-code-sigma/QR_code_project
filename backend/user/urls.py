31f7243a86e23aa6103818d49ee1d4e37ec669df
Nikitos4443 <150244714+Nikitos4443@users.noreply.github.com>
2025-03-07 14:52:39 +0200

    reset changes

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.get_users),
     path("<int:id>/", views.get_user),
     path("<int:id>/edit", views.edit_user_view),
-    path("register_for_event/<int:event_id>", views.event_registration_view),
+    path("<int:event_id>/register_for_event", views.event_registration_view),
 ]

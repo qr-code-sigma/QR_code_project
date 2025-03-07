50f1f93adbc6d0163682c143197a9ca25b92e0e0
Dania <139234410+d-kuzia@users.noreply.github.com>
2025-03-08 00:20:35 +0200

    create loader with styles

---
+++ frontend/src/components/Loading/loading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./loading.css";
+
+function Loading() {
+  return (
+    <div className="loader-container">
+      <div class="loader">
+        <div class="box1"></div>
+        <div class="box2"></div>
+        <div class="box3"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;

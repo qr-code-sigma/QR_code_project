dcb81ae12c3badc8a1af69dca503dd2d8d9f3ed6
budulka <azazasapli@gmail.com>
2025-03-07 15:46:44 +0200

    add name for get qr code url

--- backend/qr_code/urls.py
+++ backend/qr_code/urls.py
@@ -4,6 +4,6 @@ from qr_code import views
 app_name = "qr_code"
 
 urlpatterns = [
-    path("get_qr_code/<int:event_id>", views.get_qr),
+    path("get_qr_code/<int:event_id>", views.get_qr, name = "get_qr_code"),
     path("invitation/<int:event_user_id>", views.render_qr_page, name="Invitation")
 ]

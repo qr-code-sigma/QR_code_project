f6c95e56a3fe2ca6b0cec4c5ddaf6f874edf28eb
Nikitos4443 <150244714+Nikitos4443@users.noreply.github.com>
2025-03-07 21:51:56 +0200

    fix post events

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
-    count = serializers.IntegerField()
+    count = serializers.IntegerField(read_only=True)
     class Meta:
         model = Event
         fields = ['id', 'name', 'description', 'places', 'location', 'date', 'status', 'count']

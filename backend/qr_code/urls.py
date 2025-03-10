d8b45a57a8d6c9f362df33593abf0aaf1ff36a19
budulka <azazasapli@gmail.com>
2025-03-10 16:28:40 +0200

    fix invitation url

--- backend/qr_code/urls.py
+++ backend/qr_code/urls.py
@@ -5,5 +5,5 @@ app_name = "qr_code"
 
 urlpatterns = [
     path("get_qr_code/<int:event_id>", views.get_qr, name = "get_qr_code"),
-    path("invitation/<str:event_user_id>", views.render_qr_page, name="Invitation")
+    path("invitation/<str:encrypted_id>", views.render_qr_page, name="Invitation")
 ]

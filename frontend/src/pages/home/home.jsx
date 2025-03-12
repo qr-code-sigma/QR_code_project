aea847e12d0b44cf8b801eed73e5ff2f31a4e27a
DAVAdjjjan <159725111+DAVAdjjjan@users.noreply.github.com>
2025-03-12 21:39:00 +0200

    fix fetching with term

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -12,7 +12,7 @@ import { useNavigate } from "react-router-dom";
 
 function Home() {
   const { userData, isAuthenticated, getMeStatus } = useSelector(
-      (state) => state.auth,
+    (state) => state.auth,
   );
   const [events, setEvents] = useState([]);
   const [amountOfPages, setAmountOfPages] = useState(1);
@@ -22,7 +22,7 @@ function Home() {
   const [nextPageURL, setNextPageURL] = useState(null);
   const [previousPageURL, setPreviousPageURL] = useState(null);
   const navigate = useNavigate();
-  const [searchInput, setSearchInput] = useState('')
+  const [searchInput, setSearchInput] = useState("");
 
   const toAddPage = () => {
     navigate("/addEvent");
@@ -42,14 +42,13 @@ function Home() {
 
   const fetchEventsWithSearch = (term, pageNum) => {
     const url = term
-        ? `/events/${term}?page=${pageNum}`
-        : `/events?page=${pageNum}`;
+      ? `/events/${term}/?page=${pageNum}`
+      : `/events?page=${pageNum}`;
 
     fetchEvents(url).then(() => {
       console.log("Events were fetched with search term:", term);
     });
   };
-
 
   const fetchEvents = async (url) => {
     setLoading(true);
@@ -99,49 +98,53 @@ function Home() {
 
   if (getMeStatus === "resolved" || getMeStatus === "rejected") {
     return (
-        <div className="home-container">
-          {isAuthenticated ? (
-              <>
-                <Header handleSearch={handleSearch} searchInput={searchInput} setSearchInput={setSearchInput} />
-                <div className="events-container">
-                  {userData.status === "admin" && (
-                      <button onClick={toAddPage}>Add new</button>
-                  )}
+      <div className="home-container">
+        {isAuthenticated ? (
+          <>
+            <Header
+              handleSearch={handleSearch}
+              searchInput={searchInput}
+              setSearchInput={setSearchInput}
+            />
+            <div className="events-container">
+              {userData.status === "admin" && (
+                <button onClick={toAddPage}>Add new</button>
+              )}
 
-                  <Events events={events} />
-                </div>
+              <Events events={events} />
+            </div>
 
-                {!error ? (
-                    <div className="pagination-container">
-                      <button
-                          className="pagination-button"
-                          hidden={!previousPageURL}
-                          onClick={() => handlePageChange(previousPageURL, page - 1)}
-                      >
-                        <i className="fas fa-arrow-left"></i>
-                      </button>
-                      <h3>
-                        Page {page} of {amountOfPages}
-                      </h3>
-                      <button
-                          className="pagination-button"
-                          hidden={!nextPageURL}
-                          onClick={() => handlePageChange(nextPageURL, page + 1)}
-                      >
-                        <i className="fas fa-arrow-right"></i>
-                      </button>
-                    </div>
-                ) : (
-                    <div>{error}</div>
-                )}
-              </>
-          ) : (
-              <div className="content">
-                <Welcome />
+            {!error ? (
+              <div className="pagination-container">
+                <button
+                  className="pagination-button"
+                  hidden={!previousPageURL}
+                  onClick={() => handlePageChange(previousPageURL, page - 1)}
+                >
+                  <i className="fas fa-arrow-left"></i>
+                </button>
+                <h3>
+                  Page {page} of {amountOfPages}
+                </h3>
+                <button
+                  className="pagination-button"
+                  hidden={!nextPageURL}
+                  onClick={() => handlePageChange(nextPageURL, page + 1)}
+                >
+                  <i className="fas fa-arrow-right"></i>
+                </button>
               </div>
-          )}
-          <Footer />
-        </div>
+            ) : (
+              <div>{error}</div>
+            )}
+          </>
+        ) : (
+          <div className="content">
+            <Welcome />
+          </div>
+        )}
+        <Footer />
+      </div>
     );
   }
 }

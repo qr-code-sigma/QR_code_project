a5b9188598e55b970474e7a24ada9a5678074aee
Nikitos4443 <Nikitos4443@gmail.com>
2025-03-16 21:03:19 +0200

    add github ref

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="footer">
       <p>
         © 2025 Company Name. All rights reserved. The source code is available
-        on <a href="">GitHub</a>.
+        on <a href="https://github.com/qr-code-sigma/QR_code_project">GitHub</a>.
       </p>
     </footer>
   );
